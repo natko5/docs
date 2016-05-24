9cc26da4e29ec6e47034b162cf722410901cde76
Ubuntu <ubuntu@mova.institute>
2016-05-25 02:22:20 +0300

    autogenerated by https://github.com/spyysalo/jekyll-hook

--- gen_index/refresh_corpus_data.sh
+++ gen_index/refresh_corpus_data.sh
@@ -1,6 +1,6 @@
 #Where's the UD data?
-UD_DATA=$HOME/UD
-UD_TOOLS=$HOME/UD/tools
+UD_DATA=$HOME/UD_PROJHOOK/UD-dev-branches
+UD_TOOLS=$HOME/UD_PROJHOOK/tools
 
 rm -rf _corpus_data
 mkdir _corpus_data
@@ -8,6 +8,11 @@ mkdir _corpus_data
 for D in $UD_DATA/UD_*
 do
     echo "Processing $D"
+    if [[ "$1" == "pull" ]] ; then
+        pushd $D
+        git pull
+        popd
+    fi
     L=`basename $D | perl -pe s/UD_//`
     python $UD_TOOLS/conllu-stats.py --json $D/*.conllu > _corpus_data/$L.json
     echo
